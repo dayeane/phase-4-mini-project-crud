41a2e86658a3cc62b00744e67f3d9999dbf7d68a
Dayeane <dayeane85@gmail.com>
2021-12-15 15:21:18 -0800

    commit tree 6d1073ccadb26c7ca7919c40e2c3544855b67cc7

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,37 @@
+class SpicesController < ApplicationController
+  def index
+    spices = Spice.all
+    render json: spices
+  end
+
+  def create
+    new_spice = Spice.create(spice_params)
+    render json: new_spice, status: :created
+  end
+
+  def update
+    spice = Spice.find_by(id: params[:id])
+    if spice
+      spice.update(spice_params)
+      render json: spice
+    else
+      render json: "No found spice"
+    end
+  end
+
+  def destroy
+      spice = Spice.find_by(id: params[:id])
+      if spice
+        spice.destroy
+        head :no_content
+      else
+          render json: "No found spice" 
+      end
+  end
+
+  private
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+end
